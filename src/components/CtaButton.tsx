f411e5b66908497bf1c679c60281023523581a92
Andre Treib <andre.schumacher.treib@gmail.com>
2025-04-14 17:37:58 -0300

    feat: update cta color to green

--- src/components/CtaButton.tsx
+++ src/components/CtaButton.tsx
@@ -1,7 +1,7 @@
 import siteContent from "@/content/siteContent";
 
 export const ctaButtonColorCn =
-  "bg-pink-400 text-white font-medium hover:bg-pink-500 transition duration-300";
+  "bg-green-600 text-white font-medium hover:bg-green-700 transition duration-300";
 
 export function CtaButton(props: { content: string; className?: string }) {
   return (
