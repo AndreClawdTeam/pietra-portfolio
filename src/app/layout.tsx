05e9878cd478ed277f53f29c38d5606b9f87d611
Andre Treib <andre.schumacher.treib@gmail.com>
2025-05-06 21:02:39 -0300

    feat: add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -102,6 +102,43 @@ export default function RootLayout({
             `,
           }}
         />
+        <Script
+          id="gtag-1"
+          async={true}
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17059362533"
+        />
+        <Script
+          id="gtag-2"
+          async={true}
+          src="https://www.googletagmanager.com/gtag/js?id=G-JHGW1PWY5H"
+        />
+        <Script
+          id="gtag-3"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17059362533');
+              gtag('event', 'conversion', {
+                  'send_to': 'AW-17059362533/Kpf5CKe1xsMaEOXtxMY_',
+                  'value': 1.0,
+                  'currency': 'BRL'
+              });
+              gtag('config', 'G-JHGW1PWY5H');
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MRGMLF5R');
+            `,
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MRGMLF5R" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
       </body>
     </html>
   );
